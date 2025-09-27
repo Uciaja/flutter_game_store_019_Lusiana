a76f0f0a6f77adb3a8f19dba601d7ceee979a968
Uciaja <lusiwhynn15@gmail.com>
2025-09-28 04:56:13 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:game_store_019/screen/login_page.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginPage(),
+    );
+  }
+}
